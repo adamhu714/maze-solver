04730628994e27ff3b742c5c181084dd598875ed
adamhu714 <adamhu714@gmail.com>
2023-12-31 01:26:35 +0000

    Implemented lines.

--- graphics.py
+++ graphics.py
@@ -4,8 +4,8 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title('Maze Solver')
-        self.__maze = Canvas(self.__root, bg = 'white', width=width, height=height)
-        self.__maze.pack(fill=BOTH, expand=1)
+        self.__canvas = Canvas(self.__root, bg = 'white', width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
@@ -21,3 +21,23 @@ class Window:
         
     def close(self):
         self.__running = False
+
+    def drawLine(self, line, fillColour):
+        line.draw(self.__canvas, fillColour)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2) -> None:
+        self.x1 = point1.x
+        self.y1 = point1.y
+        self.x2 = point2.x
+        self.y2 = point2.y
+
+    def draw(self, canvas, fillColour):
+        canvas.create_line(self.x1, self.y1, 
+                           self.x2, self.y2, 
+                           fill=fillColour, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    p1 = Point(10, 10)
+    p2 = Point(10, 590)
+    line1 = Line(p1, p2)
+    win.drawLine(line1, "purple")
     win.wait_for_close()
 
 main()
