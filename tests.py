e0ba56cbc11138466d00eea82c3960e087780c12
adamhu714 <adamhu714@gmail.com>
2024-01-01 21:40:35 +0000

    Created the maze

--- tests.py
+++ tests.py
@@ -32,14 +32,16 @@ class Tests(unittest.TestCase):
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        self.assertEqual(
-            m1._cells[0][0].has_top_wall,
-            False,
-        )
-        self.assertEqual(
-            m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall,
-            False,
-        )
+        self.assertFalse(m1._cells[0][0].has_top_wall)
+        self.assertFalse(m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall)
+
+    def test_maze_reset_cells_visted(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        for col in m1._cells:
+            for cell in col:
+                self.assertFalse(cell.visited)
 
 if __name__ == "__main__":
     unittest.main()
